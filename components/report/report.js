c6f7df7f88f28720798bcee6db6e438aecb21c1f
Matthew Bouchard <bouchard.matthewj@gmail.com>
2017-04-26 13:34:18 -0600

    good place for presentation

---
+++ components/report/report.js
@@ -0,0 +1,6 @@
+angular.module("fantasy401k").directive('mbReport', function() {
+  return {
+          restrict: 'AE',
+          templateUrl: '/components/report/report.html',
+        }
+  })
--- module.js
+++ module.js
@@ -1,47 +1,117 @@
 'use strict'
-angular.module('fantasy401k', ['ui.router'])
-  .config(function($stateProvider, $urlRouterProvider) {
+angular.module('fantasy401k', ['ui.router', 'zingchart-angularjs']).config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
 
     $stateProvider
 
-        // HOME STATES AND NESTED VIEWS ========================================
+    // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
-            url: '/',
-            templateUrl: './partials/home.html'
-        })
+        url: '/',
+        templateUrl: './partials/home.html'
+    })
 
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+    // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('about', {
-            url: '/about',
-            templateUrl: './partials/about.html'
+        url: '/about',
+        templateUrl: './partials/about.html'
 
-        })
-        .state('dashboard', {
-            url: '/dashboard',
-            templateUrl: './partials/dashboard-view.html'
+    }).state('dashboard', {
+        url: '/dashboard',
+        templateUrl: './partials/dashboard-view.html'
 
-        })
+    })
+    .state('graphs', {templateUrl: './partials/graphs.html'})
+    .state('graphs.profile', {
+        url: '/profile/base',
+        //template: '<mb-report title={{info.title}} image={{info.image}} data={{info.data}} </mb-report>',
+        templateUrl: './partials/graphs.base.html',
+        controller: function($scope) {
+            $scope.info = {}
+            $scope.info.title = "User's Base Profile";
+            $scope.info.image = '/assets/photos/balanced.png';
+            $scope.data = {
+                type: 'line',
+                series: [
+                    {
+                        values: [54, 23, 34, 23, 43,20,19,37]
+                    }, {
+                        values: [10, 15, 16, 20, 40,45,17,47]
+                    },
+                    {
+                        values: [9,13,17,23,29,31,37,41]
+                    }
+                ]
+            };
 
+        }
+    })
+    .state('graphs.moderate', {
+        url: '/profile/moderate',
+        templateUrl: './partials/graphs.moderate.html',
+        controller: function($scope) {
+            $scope.info = {}
+            $scope.info.title = "User's Moderate Profile";
+            $scope.info.image = '/assets/photos/moderate.png';
+            $scope.data = {
+                type: 'line',
+                series: [
+                    {
+                      values: [10, 15, 16, 20, 40,45,17,47]
+                    }, {
+                      values: [54, 23, 34, 23, 43,20,19,37]
+                    },
+                    {
+                        values: [9, 13, 17, 23, 29, 31, 37, 41 ]
+                    }
+                ]
+            };
+
+        }
+    })
+    .state('graphs.stable', {
+        url: '/profile/stable',
+        templateUrl: './partials/graphs.stable.html',
+        controller: function($scope) {
+            $scope.info = {}
+            $scope.info.title = "User's Stable Profile";
+            $scope.info.image = '/assets/photos/stable.png';
+            $scope.data = {
+                type: 'line',
+                series: [
+                    {
+                        values: [54, 23, 34, 23, 43,20,19,37]
+                    }, {
+                      values: [9,13,17,23,29,31,37,41]
+                    },
+                    {
+                      values: [10, 15, 16, 20, 40,45,17,47]
+                    }
+                ]
+            };
+
+        }
+    })
+    .state('graphs.aggressive', {
+        url: '/profile/aggressive',
+        templateUrl: './partials/graphs.aggressive.html',
+        controller: function($scope) {
+            $scope.info = {}
+            $scope.info.title = "User's Aggressive Profile";
+            $scope.info.image = '/assets/photos/aggressive.png';
+            $scope.data = {
+                type: 'line',
+                series: [
+                    {
+                      values: [9,13,17,23,29,31,37,41]
+                    }, {
+                      values: [10, 15, 16, 20, 40,45,17,47]
+                    },
+                    {
+                      values: [54, 23, 34, 23, 43,20,19,37]
+                    }
+                ]
+            };
+
+        }
+    });
 });
-
-    // .config(function($routeProvider, $locationProvider) {
-    //     $routeProvider
-    //         .when('/', {
-    //             templateUrl: './partials/home.html',
-    //             controller: function($scope) {
-    //                 $scope.test = "hello world";
-    //             }
-    //         })
-    //         .when('/about', {
-    //             templateUrl: './partials/about.html'
-    //             // controller: function($scope) {
-    //             //     $scope.test = "hello world";
-    //             // }
-    //         })
-    //         .otherwise({
-    //             redirectTo: '/'
-    //         });
-    //
-    //     // $locationProvider.html5Mode(true);
-    // });
